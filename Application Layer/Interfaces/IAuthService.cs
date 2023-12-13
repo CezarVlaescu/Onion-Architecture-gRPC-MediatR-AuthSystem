eb0feeb559363636322e93e99ffb56b09257649d
Vlaescu Cezar <vlaescucezar@yahoo.com>
2023-12-13 18:38:53 +0200

    First

---
+++ Application Layer/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Auth0.ManagementApi.Models.Rules;
+using SlackAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Layer.Interfaces
+{
+    public interface IAuthService
+    {
+        //Task<LoginResponse> LoginAsync(LoginRequest request); for GRPC
+
+    }
+}
---
+++ GrpcAuthService/Services/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using Auth;
+using Grpc.Core;
+
+namespace GrpcAuthService.Services
+{
+    public class AuthService : Auth.AuthService
+    {
+        private readonly ILogger<AuthService> _logger;
+
+        public AuthService(ILogger<AuthService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<LoginResponse> LoginResponse(LoginRequest request, ServerCallContext context) 
+        {
+            try
+            {
+                _logger.LogInformation(request.Username, request.Password);
+
+                return Task.FromResult(new LoginResponse
+                {
+                    Success = true,
+                    Messsage = "Succesfull logged",
+                    Token = " "
+                });
+            } catch
+            {
+                throw new Exception("Not success");
+            }
+
+        }
+    }
+}
