73508dddab56b03f232320d4c2a511194386c682
Cosmin <cosadi2002@gmail.com>
2024-03-22 11:16:01 +0200

    #1 - Implemented LoginDto, LoginUserCommand, LoginUserCommandHandler in application layer, Implemented UserLoginResult in core layer (foarte asemanatoare cu cele de la register), changed IUserService to accomodate the login module but not yet implemented in UserService

---
+++ Application Layer/Dtos/Auth/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Layer.Dtos.Auth
+{
+    public class LoginDto
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string? Password { get; set; }
+    }
+}
